3c79fe87f40f574502028fbdede8378e2921d1d7
Justin de Leon <jusmdeleon@gmail.com>
2015-11-16 15:42:31 +0800

    Add class for managing domains

---
+++ src/Domain.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Coreproc\Enom;
+
+class Domain
+{
+
+    /**
+     * @var Enom
+     */
+    private $enom;
+
+    private $client;
+
+    public function __construct(Enom $enom)
+    {
+        $this->enom = $enom;
+        $this->client = $enom->getClient();
+    }
+
+    public function check($sld, $tld)
+    {
+        $response = $this->doGetRequest('check', [
+            'sld' => $sld,
+            'tld' => $tld,
+        ]);
+
+        return $this->parseXMLObject($response);
+    }
+
+    public function purchase($sld, $tld)
+    {
+        $response = $this->doGetRequest('Purchase', [
+            'sld' => $sld,
+            'tld' => $tld,
+        ]);
+
+        return $this->parseXMLObject($response);
+    }
+
+    public function getStatus($sld, $tld, $orderId)
+    {
+        $response = $this->doGetRequest('GetDomainStatus', [
+            'sld'       => $sld,
+            'tld'       => $tld,
+            'orderid'   => $orderId,
+            'ordertype' => 'purchase',
+        ]);
+
+        return $this->parseXMLObject($response);
+    }
+
+    public function getList()
+    {
+        $response = $this->doGetRequest('GetDomains');
+
+        return $response;
+    }
+
+    public function getExpired()
+    {
+        $response = $this->doGetRequest('GetExpiredDomains');
+
+        return $response;
+    }
+
+    public function getInfo($sld, $tld)
+    {
+        $response = $this->doGetRequest('GetDomainInfo', [
+            'sld' => $sld,
+            'tld' => $tld
+        ]);
+
+        return $this->parseXMLObject($response);
+    }
+
+    private function doGetRequest($command, $additionalParams = [])
+    {
+        $params = [
+            'command' => $command,
+        ];
+
+        if (count($additionalParams)) {
+            $params = array_merge($params, $additionalParams);
+        }
+
+        return $this->client->get('', ['query' => $params])->xml();
+    }
+
+    private function parseXMLObject($object)
+    {
+        return json_decode(json_encode($object));
+    }
+}
